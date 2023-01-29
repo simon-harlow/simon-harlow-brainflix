bdcf9be605e24c29c55711333d515f993f85b252
Simon Harlow <simonharlow11@gmail.com>
2023-01-28 19:05:11 -0800

    finished creating file structure, started work on Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.scss'
+import Logo from '../../assets/images/BrainFlix-logo.svg'
+import Avatar from '../../assets/images/Mohan-muruge.jpg'
+
+export default function Header() {
+    return (
+        <>
+            <header className="header-container">
+                <div className="header">
+                    <div className="header__logo">
+                        <img className="header__logo--img" src={Logo} alt="brainflix logo"/>
+                    </div>
+                    <div className="header__search">
+                        <form>
+                            <input type="text" value="Search"/>
+                        </form>
+                    </div>
+                    <div className="header__avatar">
+                        <img className="header__avatar--img"src={Avatar} alt="user avatar"/>
+                    </div>
+                    <div className="header__button">
+                        <button className="header__button--btn">Upload</button>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
