0340467a23a129cd657e610d097c3f468136486a
Simon Harlow <simonharlow11@gmail.com>
2023-02-17 16:48:39 -0800

    Styled Upload page for tablet and desktop view in scss

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -7,27 +7,35 @@ import Publish from '../../assets/images/publish.svg'
 export default function Upload() {
     return (
     <>
-        <section className="upload">
-            <div className="upload__title">
-                <h1 className="upload__title-text">Upload Video</h1>
-            </div>
-            <form className="upload__form" id="upload-form">
-                <label className="upload__label" htmlFor="thumbnail">VIDEO THUMBNAIL</label>
-                <img className="upload__thumbnail" src={VideoThumbnail} alt="bike handlebars" name="thumbnail" id="thumbnail"/>
-                <label className="upload__label" htmlFor="title">TITLE YOUR VIDEO</label>
-                <input className="upload__input" type="text" name="title" id="title" maxlength="40" placeholder="Add a title to your video" />
-                <label className="upload__label" htmlFor="description">ADD A VIDEO DESCRIPTION</label>
-                <textarea className="upload__input upload__input--textarea" name="description" id="description" maxlength="200" placeholder="Add a description to your video" />
-                <button className="upload__publish-button" type="submit" id="submit">
-                    <img className="upload__publish-button-img" src={Publish} alt="publish icon" />
-                    <span className="upload__publish-button-text">PUBLISH</span>
-                </button>
-                <button className="upload__cancel-button" type="button" id="cancel">
-                    <img className="upload__cancel-button-img" src={Publish} alt="publish icon" />
-                    <span className="upload__cancel-button-text">CANCEL</span>
-                </button>
-            </form>
-        </section>
+        <main className="upload-container">
+            <section className="upload">
+                <div className="upload__title">
+                    <h1 className="upload__title-text">Upload Video</h1>
+                </div>
+                <form className="upload-form" id="upload-form">
+                    <div className="upload-form__thumbnail-container">
+                        <label className="upload-form__label upload-form__label--thumbnail" htmlFor="thumbnail">VIDEO THUMBNAIL</label>
+                        <img className="upload-form__thumbnail" src={VideoThumbnail} alt="bike handlebars" name="thumbnail" id="thumbnail"/>
+                    </div>
+                    <div className="upload-form__input-container">
+                        <label className="upload-form__label" htmlFor="title">TITLE YOUR VIDEO</label>
+                        <input className="upload-form__input" type="text" name="title" id="title" maxlength="40" placeholder="Add a title to your video" />
+                        <label className="upload-form__label" htmlFor="description">ADD A VIDEO DESCRIPTION</label>
+                        <textarea className="upload-form__input upload-form__input--textarea" name="description" id="description" maxlength="200" placeholder="Add a description to your video" />
+                    </div>
+                    <div className="upload-form__button-container">
+                        <button className="upload-form__publish-button" type="submit" id="submit">
+                            <img className="upload-form__publish-button-img" src={Publish} alt="publish icon" />
+                            <span className="upload-form__publish-button-text">PUBLISH</span>
+                        </button>
+                        <button className="upload-form__cancel-button" type="button" id="cancel">
+                            <img className="upload-form__cancel-button-img" src={Publish} alt="publish icon" />
+                            <span className="upload-form__cancel-button-text">CANCEL</span>
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </main>
     </>
     )
 }
